f543dc74ff121894e84a218ae792422f35991b11
Shankar1728 <neelapalashankar876@gmail.com>
2025-07-01 17:23:11 +0530

    commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,20 +6,24 @@ function Projects() {
     <div className="projects-container">
       <div className="project-card">
         <h2 className="project-title">Brain Tumor Detection</h2>
-        <p className="project-description">
-          Designed and implemented a CNN-based deep learning model for brain tumor detection from MRI images.
-          Trained on curated medical imaging datasets, the model demonstrated high accuracy and robust evaluation performance.
-        </p>
+        <div className="project-description">
+          <p>
+            Designed and implemented a CNN-based deep learning model for brain tumor detection from MRI images.
+            Trained on curated medical imaging datasets, the model demonstrated high accuracy and robust evaluation performance.
+          </p>
+        </div>className="project-description"
         <a href="https://github.com/Shankar1728/BrainTumor_Detection" target="_blank" className="project-link">
           View Project
         </a>
       </div>
       <div className="project-card">
         <h2 className="project-title">Portfolio Website</h2>
-        <p className="project-description">
-          A simple and elegant portfolio website developed with react, featuring my skills, education and 
-          projects in a clean and responsive layout.
-        </p>
+        <div className="project-description">
+          <p>
+            A simple and elegant portfolio website developed with react, featuring my skills, education and 
+            projects in a clean and responsive layout.
+          </p>
+        </div>
         <a href="https://github.com/Shankar1728/Portfolio" target="_blank" className="project-link">
           View Project
         </a>
