fc7a1ae22b7aa5e5d563d93007c4f725be1c4ffb
Shankar1728 <neelapalashankar876@gmail.com>
2025-07-01 17:13:13 +0530

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,15 +17,14 @@ function Contact() {
         <table className="contact-table">
           <tbody>
             <tr>
-              <td>Email</td>
-              <td>:</td>
+              <th>Email</th>
               <td>
                 <a href="mailto:neelapalashankar876@gmail.com">neelapalashankar876@gmail.com</a>
               </td>
             </tr>
             <tr>
-              <td>Phone Number</td>
-              <td>:</td>
+              <th>Phone Number</th>
+    
               <td>
                 <span id="phone-number">+91 8309209715</span>
                 <button onClick={handleCopy} className="copy-btn">Copy</button>
@@ -33,31 +32,28 @@ function Contact() {
               </td>
             </tr>
             <tr>
-              <td>LinkedIn</td>
-              <td>:</td>
+              <th>LinkedIn</th>
+    
               <td>
-                <a href="https://www.linkedin.com/in/shankar-neelapala-43b04628b/" target="_blank">
-                  https://www.linkedin.com/in/shankar-neelapala-43b04628b/
+                <a href="https://www.linkedin.com/in/shankar-neelapala-43b04628b/" target="_blank" style={{textDecoration:'none'}}>
+                  https://www.linkedin.com/in/shankar-neelapala-43b04628b
                 </a>
               </td>
             </tr>
             <tr>
-              <td>GitHub</td>
-              <td>:</td>
+              <th>GitHub</th>
               <td>
-                <a href="https://github.com/Shankar1728" target="_blank">
+                <a href="https://github.com/Shankar1728" target="_blank" style={{textDecoration:'none'}}>
                   https://github.com/Shankar1728
                 </a>
               </td>
             </tr>
             <tr>
-              <td>Address</td>
-              <td>:</td>
+              <th>Address</th>
               <td>Gollagudem near Pedakapavaram, Akividu Mandal, West Godavari, Andhra Pradesh, India</td>
             </tr>
             <tr>
-              <td>PinCode</td>
-              <td>:</td>
+              <th>PinCode</th>
               <td>534235</td>
             </tr>
           </tbody>
