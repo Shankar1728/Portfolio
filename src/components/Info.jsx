b4ce8c000515dc90e95a295bd12273858cc6b1b8
Shankar1728 <neelapalashankar876@gmail.com>
2025-08-19 17:22:02 +0530

    commit

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,9 +4,12 @@ function Info() {
   return (
     <div id="info-div">
       <div><h1><b>I am Shankara Vara Prasad Neelapala</b></h1></div>
-        <div>
+        <div style={{gap:'10px', display:'flex'}}>
             <a href='/shankar_resume.pdf' target="_blank">
                 <button type='submit' id="button-id">Resume</button>
+            </a>
+            <a href='/shankar_resume.pdf' download="Shankar_resume.pdf">
+                <b style={{fontSize:'20px'}}> Download Resume</b>
             </a>
         </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,19 +18,16 @@ function Projects() {
       </div>
       <div className="project-card">
         <h2 className="project-title">Portfolio Website</h2>
-        <p className="project-description">
-          A simple and elegant portfolio website developed with react, featuring my skills, education and projects in a clean
-          and responsive layout.
-        </p>
-        <a href="https://github.com/Shankar1728/BrainTumor_Detection" target="_blank" className="project-link"/>
-          Github Repository
         <div className="project-description">
           <p>
-            A simple and elegant portfolio website developed with react, featuring my skills, education ,
-            projects and my contact details in a clean and responsive layout. 
+            A simple and elegant portfolio website developed with react, featuring my skills, education and projects in a clean
+            and responsive layout.
           </p>
         </div>
-      </div>
+        <a href="https://github.com/Shankar1728/BrainTumor_Detection" target="_blank" className="project-link">
+          Github Repository
+          </a>
+        </div>
     </div>
   );
 }
